641eddc72d812a9462782146e479b68431e71078
bart.lee <bart.lee@daumkakao.com>
2017-02-22 18:43:58 +0900

    init

---
+++ src/main/java/org/kow/KowServeApplication.java
@@ -0,0 +1,12 @@
+package org.kow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KowServeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KowServeApplication.class, args);
+	}
+}
