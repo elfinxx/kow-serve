cab43994544530275d0ba94f754e3e98c7d69fbc
bart.lee <bart.lee@daumkakao.com>
2017-02-22 19:56:35 +0900

    Google docs 테스

---
+++ src/main/java/org/kow/controller/TestController.java
@@ -0,0 +1,34 @@
+package org.kow.controller;
+
+import com.google.api.services.sheets.v4.Sheets;
+import com.google.api.services.sheets.v4.model.ValueRange;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.util.List;
+
+import static org.kow.util.GoogleSheetHelper.getSheetsService;
+
+@RestController
+@RequestMapping("/api")
+public class TestController {
+    private Logger logger = LoggerFactory.getLogger(getClass());
+
+    @RequestMapping(value = "/test")
+    public String test() throws IOException {
+        // Build a new authorized API client service.
+        Sheets service = getSheetsService();
+
+        String spreadsheetId = "1D63gLoHAt2l_yaW-F47pkjsqq-82JlGKZ5wXolEj-fI";
+        String range = "test!A2:E";
+        ValueRange response = service.spreadsheets().values()
+                .get(spreadsheetId, range)
+                .execute();
+        List<List<Object>> values = response.getValues();
+        logger.info(values.toString());
+        return "HELL";
+    }
+}
